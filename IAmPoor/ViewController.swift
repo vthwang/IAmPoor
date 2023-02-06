d45441c621363bb1a3624d4821b71538c06bf3e2
TingSyuanWang <vincent@fishboneapps.com>
2023-02-06 16:53:01 -0500

    Initial Commit

---
+++ IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Ting-Syuan Wang on 2023/2/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
